84004727cfca372fe98d86502fb3246fadf6ac3b
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-08-26 16:15:29 +0900

    add

--- beauty/app/src/main/java/com/yijun/beauty/Address.java
+++ beauty/app/src/main/java/com/yijun/beauty/Address.java
@@ -1,17 +1,63 @@
 package com.yijun.beauty;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class Address extends AppCompatActivity {
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.MarkerIcons;
 
-    private String ClientId = "9s3u08ar9q";
-    private String ClientSecret = "jpOR5z1HPbPxX2nLP9CJwF03RQZ2UoxVdyFoiRJ3";
+import java.util.Map;
+
+public class Address extends FragmentActivity implements OnMapReadyCallback {
+
+    Map map;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address);
+
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+        LatLng coord = new LatLng(37.5593,126.8362133);
+
+        Toast.makeText(Address.this,
+                "위도: " + coord.latitude + ", 경도: " + coord.longitude,
+                Toast.LENGTH_SHORT).show();
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true);
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true);
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5593, 126.8362133));
+        marker.setIcon(MarkerIcons.RED);
+        marker.setIconTintColor(Color.RED);
+        marker.setMap(naverMap);
+
     }
 }
