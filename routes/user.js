adbcb5ede28778ab972c847562c0eb0226a78d63
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-08-27 10:37:47 +0900

    add

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createUser, loginUser, logoutUser } = require("../controller/user");
-const router = express.Router();
-
-router.route("/add").post(createUser);
-router.route("/login").post(loginUser);
-router.route("/logout").delete(logoutUser);
-
-module.exports = router;
