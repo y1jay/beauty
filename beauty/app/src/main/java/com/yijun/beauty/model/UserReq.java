314b57b964a593b7b15ab714c58348106b105a00
y1jay <67768207+y1jay@users.noreply.github.com>
2020-08-27 19:42:56 +0900

    addd

--- beauty/app/src/main/java/com/yijun/beauty/model/UserReq.java
+++ beauty/app/src/main/java/com/yijun/beauty/model/UserReq.java
@@ -9,11 +9,6 @@ public class UserReq {
     public UserReq() {
     }
 
-    public UserReq(String name, String passwd, String phone) {
-        this.name = name;
-        this.passwd = passwd;
-        this.phone = phone;
-    }
 
     public UserReq(String nick_name, String passwd) {
         this.nick_name = nick_name;
