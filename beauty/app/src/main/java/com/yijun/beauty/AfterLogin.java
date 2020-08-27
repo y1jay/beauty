c3d3e61e3b2fe5e55c540d48c66915e7c619305c
y1jay <67768207+y1jay@users.noreply.github.com>
2020-08-27 16:21:02 +0900

    add

--- beauty/app/src/main/java/com/yijun/beauty/AfterLogin.java
+++ beauty/app/src/main/java/com/yijun/beauty/AfterLogin.java
@@ -1,11 +1,15 @@
 package com.yijun.beauty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -91,5 +95,27 @@ Button address;
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menus, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.myInfo){
+
+            return true;
+        }else if (id == R.id.reservation_check){
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
