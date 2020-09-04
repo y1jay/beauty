3d1fedab851d660f4d700f72874d5cb9e87e4a86
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-09-04 18:06:52 +0900

    add

--- beauty/app/src/main/java/com/yijun/beauty/AfterLogin.java
+++ beauty/app/src/main/java/com/yijun/beauty/AfterLogin.java
@@ -35,6 +35,7 @@ Button address;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_login);
+
 //        Session session = Session.getCurrentSession();
 //        session.open(AuthType.KAKAO_LOGIN_ALL, AfterLogin.this);
 //
