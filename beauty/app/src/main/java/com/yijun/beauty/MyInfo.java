6a967bf10bab8fca6f996c5ca4e19dcce3e69f95
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-09-08 13:16:22 +0900

    add

--- beauty/app/src/main/java/com/yijun/beauty/MyInfo.java
+++ beauty/app/src/main/java/com/yijun/beauty/MyInfo.java
@@ -3,13 +3,20 @@ package com.yijun.beauty;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.kakao.auth.ApiErrorCode;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.UnLinkResponseCallback;
 import com.yijun.beauty.api.NetworkClient;
 import com.yijun.beauty.api.UserApi;
 import com.yijun.beauty.model.UserRes;
@@ -22,50 +29,63 @@ import retrofit2.Retrofit;
 
 public class MyInfo extends AppCompatActivity {
 
-    Button btn_die;
-    SharedPreferences sp;
+    Button btn_end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_info);
 
-
-        btn_die = findViewById(R.id.btn_die);
-        btn_die.setOnClickListener(new View.OnClickListener() {
+        btn_end = findViewById(R.id.btn_end);
+        btn_end.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sp=getSharedPreferences(
-                        Utils.PREFERENCES_NAME,MODE_PRIVATE);
-                final String token = sp.getString("token",null);
+                new AlertDialog.Builder(MyInfo.this)
+                        .setMessage("탈퇴하시겠습니까?")
+                        .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //"예" 버튼 클릭시 할 동작
+                                UserManagement.getInstance().requestUnlink(new UnLinkResponseCallback() { //회원탈퇴 실행
+                                    @Override
+                                    public void onSessionClosed(ErrorResult errorResult) { //로그인 세션이 닫혀있을 시
+                                        //다시 로그인해달라는 Toast 메세지를 띄우고 로그인 창으로 이동함
+                                        Toast.makeText(getApplicationContext(), "로그인 세션이 닫혔습니다. 다시 로그인해 주세요.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(MyInfo.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
 
-                Retrofit retrofit = NetworkClient.getRetrofitClient(MyInfo.this);
-                UserApi userApi = retrofit.create(UserApi.class);
-                Call<UserRes> call = userApi.delUser("Bearer "+token);
-                call.enqueue(new Callback<UserRes>() {
-                    @Override
-                    public void onResponse(Call<UserRes> call, Response<UserRes> response) {
-                        if (response.isSuccessful()){
-                            if (response.body().isSuccess()){
-                                sp = getSharedPreferences(Utils.PREFERENCES_NAME,MODE_PRIVATE);
-                                SharedPreferences.Editor editor = sp.edit();
-                                editor.putString("token",null);
-                                editor.putBoolean("auto_login",false);
-                                editor.apply();
-                                Intent i = new Intent(MyInfo.this,MainActivity.class);
-                                i.putExtra("key",1);
+                                    @Override
+                                    public void onFailure(ErrorResult errorResult) { //회원탈퇴 실패 시
+                                        int result = errorResult.getErrorCode();
 
-                                finish();
-                                startActivity(i);
+                                        if (result == ApiErrorCode.CLIENT_ERROR_CODE) {
+                                            Toast.makeText(getApplicationContext(), "네트워크 연결이 불안정합니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "회원탈퇴에 실패했습니다. 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                    @Override
+                                    public void onSuccess(Long result) { //회원탈퇴에 성공하면
+                                        //"회원탈퇴에 성공했습니다."라는 Toast 메세지를 띄우고 로그인 창으로 이동함
+                                        Toast.makeText(getApplicationContext(), "회원탈퇴에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(MyInfo.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                });
+
+                                dialog.dismiss(); //팝업창 종료
                             }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<UserRes> call, Throwable t) {
-                        Log.i("AAAA","? ",t);
-                    }
-                });
+                        })
+                        .setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //"아니오" 버튼 클릭 시 할 동작
+                                dialog.dismiss(); //팝업창 종료
+                            }
+                        }).show();
             }
         });
 
