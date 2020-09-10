b8fd818a78895e5d20124564454d6d977d3e5be0
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-09-10 15:10:41 +0900

    add

--- beauty/app/src/main/java/com/yijun/beauty/url/Utils.java
+++
@@ -1,7 +0,0 @@
-package com.yijun.beauty.url;
-
-public class Utils {
-
-    public static final  String BASE_URL = "http://photoserver-env.eba-tbvnbpms.ap-northeast-2.elasticbeanstalk.com";
-    public static final String PREFERENCES_NAME = "Bearer";
-}
