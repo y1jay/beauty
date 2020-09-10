3f4cac42263fe179e8acda57d90a84ac43a07710
nerd444 <67768201+nerd444@users.noreply.github.com>
2020-09-10 21:47:39 +0900

    add

--- beauty/app/src/main/java/com/yijun/beauty/Reservation.java
+++ beauty/app/src/main/java/com/yijun/beauty/Reservation.java
@@ -4,20 +4,37 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yijun.beauty.activity.CheckoutActivity;
+import com.yijun.beauty.api.NetworkClient;
+import com.yijun.beauty.api.ReservationApi;
+import com.yijun.beauty.api.UserApi;
+import com.yijun.beauty.model.ReservationReq;
+import com.yijun.beauty.model.ReservationRes;
+import com.yijun.beauty.model.UserReq;
+import com.yijun.beauty.model.UserRes;
+import com.yijun.beauty.url.Utils;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class Reservation extends AppCompatActivity {
 
+    SharedPreferences sp;
     Button btn_payment;
     
     CheckBox check_main_menu1;
@@ -45,6 +62,8 @@ public class Reservation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
 
+        sp = getSharedPreferences(Utils.PREFERENCES_NAME,MODE_PRIVATE);
+
         check_main_menu1 = findViewById(R.id.check_main_menu1);
         main_menu1 = findViewById(R.id.main_menu1);
         pay_main1 = findViewById(R.id.pay_main1);
@@ -57,50 +76,20 @@ public class Reservation extends AppCompatActivity {
             String main = main_menu1.getText().toString().trim();
             String pay = pay_main1.getText().toString().trim();
 
-            menuArrayList.add(0, main);
-            priceArrayList.add(0, pay);
+            add_menu(main, pay);
         }
 
         if (check_main_menu2.isChecked() == true){
             String main = main_menu2.getText().toString().trim();
             String pay = pay_main2.getText().toString().trim();
 
-            menuArrayList.add(1, main);
-            priceArrayList.add(2, pay);
+            add_menu(main, pay);
         }
 
         btn_payment = findViewById(R.id.btn_payment);
         btn_payment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                if (check_main_menu1.isChecked() == true){
-//                    String main = main_menu1.getText().toString().trim();
-//                    String pay = pay_main1.getText().toString().trim();
-//
-//                    Intent i = new Intent(Reservation.this, CheckoutActivity.class);
-//                    i.putExtra("main", main);
-//                    i.putExtra("pay", pay);
-//                    startActivity(i);
-//                }
-//                AlertDialog.Builder alert = new AlertDialog.Builder(Reservation.this);
-//                View alertView = getLayoutInflater().inflate(R.layout.order,null);
-//                menu = alertView.findViewById(R.id.menu);
-//                price = alertView.findViewById(R.id.price);
-//                total = alertView.findViewById(R.id.total);
-//                order_no = alertView.findViewById(R.id.order_no);
-//                order_payment = alertView.findViewById(R.id.order_payment);
-//
-//                String menu1 = menuArrayList.get(0).toString();
-//                String price1 = priceArrayList.get(0).toString();
-//
-//                String menu2 = menuArrayList.get(1).toString();
-//                String price2 = priceArrayList.get(1).toString();
-//
-//                menu.setText(menu1+"\n"+menu2);
-//                price.setText(price1+"\n"+price2);
-//
-//                total.setText(price1+price2);
-
                 AlertDialog.Builder alert = new AlertDialog.Builder(Reservation.this);
                 View alertView = getLayoutInflater().inflate(R.layout.order,null);
                 menu = alertView.findViewById(R.id.menu);
@@ -109,15 +98,7 @@ public class Reservation extends AppCompatActivity {
                 order_no = alertView.findViewById(R.id.order_no);
                 order_payment = alertView.findViewById(R.id.order_payment);
 
-                String main = main_menu1.getText().toString().trim();
-                String pay = pay_main1.getText().toString().trim();
-                if (check_main_menu1.isChecked() == true){
-
-                    menu.setText(main);
-                    String nowon = pay.replace("원", "");
-                    price.setText(nowon);
-                    total.setText(pay);
-                }
+                select_menu();
 
                 order_payment.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -144,8 +125,56 @@ public class Reservation extends AppCompatActivity {
                 alertDialog.show();
             }
         });
-
-
-
     }
+
+    public void add_menu(String menu, String price){
+        String nick_name =sp.getString("nick_name",null);
+
+        ReservationReq reservationReq = new ReservationReq(menu, price, nick_name);
+
+        Retrofit retrofit = NetworkClient.getRetrofitClient(Reservation.this);
+        ReservationApi reservationApi = retrofit.create(ReservationApi.class);
+
+        Call<ReservationRes> call = reservationApi.addMenu(reservationReq);
+
+        call.enqueue(new Callback<ReservationRes>() {
+            @Override
+            public void onResponse(Call<ReservationRes> call, Response<ReservationRes> response) {
+                // 상태코드가 200 인지 확인
+                if (response.isSuccessful()){
+                    Intent i = new Intent(Reservation.this, CheckoutActivity.class);
+                    i.putExtra("menu", menu);
+                    i.putExtra("price", price);
+                    startActivity(i);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ReservationRes> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void select_menu(){
+        String nick_name =sp.getString("nick_name",null);
+
+        Retrofit retrofit = NetworkClient.getRetrofitClient(Reservation.this);
+        ReservationApi reservationApi = retrofit.create(ReservationApi.class);
+
+        Call<ReservationRes> call = reservationApi.selectMenu(nick_name);
+
+        call.enqueue(new Callback<ReservationRes>() {
+            @Override
+            public void onResponse(Call<ReservationRes> call, Response<ReservationRes> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<ReservationRes> call, Throwable t) {
+
+            }
+        });
+    }
+
 }
